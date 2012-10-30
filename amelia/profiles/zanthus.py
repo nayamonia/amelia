18dfb97e8c6ee49043fceb09c11dbf920dca2ae3
nayamonia <gabriel@duel.com.br>
2012-10-30 11:13:49 -0200

    amelia profiles

---
+++ amelia/profiles/zanthus.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+# Configs para PDV Zanthus
+# Arquivos de configuração
+
+HEADER = {"name" : "Zanthus",
+          "description" : "Perfil de configuração padrão",
+          "icon" : "zeus.gif",}
+
+ECF9CFG = {"name" : "ECF9.CFG",
+           "path" : "/Zanthus/Zeus/pdvJava",
+           "summary" : "Arquivo de configuração",
+           "description" : """
+               Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do 
+               eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut 
+               enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+               nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
+               reprehenderit in voluptate velit esse cillum dolore eu fugiat 
+               nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
+               sunt in culpa qui officia deserunt mollit anim id est laborum.
+           """,}
+
+ECF9ACFG = {"name" : "ECFA9.CFG",
+            "path" : "/Zanthus/Zeus/pdvJava",
+            "summary" : "Arquivo de configuração A",
+            "description" : """
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do 
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut 
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
+                reprehenderit in voluptate velit esse cillum dolore eu fugiat 
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+            """,}
+
+ECF9BCFG = {"name" : "ECF9B.CFG",
+            "path" : "/Zanthus/Zeus/pdvJava",
+            "summary" : "Arquivo de configuração B",
+            "description" : """
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do 
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut 
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
+                reprehenderit in voluptate velit esse cillum dolore eu fugiat 
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+            """,}
+
+ECF9CCFG = {"name" : "ECF9C.CFG",
+            "path" : "/Zanthus/Zeus/pdvJava",
+            "summary" : "Arquivo de configuração C",
+            "description" : """
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do 
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut 
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
+                reprehenderit in voluptate velit esse cillum dolore eu fugiat 
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+            """,}
+
+ECF9DCFG = {"name" : "ECF9D.CFG",
+            "path" : "/Zanthus/Zeus/pdvJava",
+            "summary" : "Arquivo de configuração D",
+            "description" : """
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do 
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut 
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
+                reprehenderit in voluptate velit esse cillum dolore eu fugiat 
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+            """,}
+
+ECF9FCFG = {"name" : "ECF9F.CFG",
+            "path" : "/Zanthus/Zeus/pdvJava",
+            "summary" : "Arquivo de configuração F",
+            "description" : """
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do 
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut 
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
+                reprehenderit in voluptate velit esse cillum dolore eu fugiat 
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+            """,}
+
+CLISITEF = {"name" : "CliSiTef.ini",
+            "path" : "/Zanthus/Zeus/pdvJava",
+            "summary" : "Arquivo de configuração da Clisitef",
+            "description" : """
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do 
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut 
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
+                reprehenderit in voluptate velit esse cillum dolore eu fugiat 
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+            """,}
+
+CONFIG_FILES = [ECF9CFG, ECF9ACFG, ECF9BCFG, ECF9CCFG, ECF9DCFG, ECF9FCFG, 
+                CLISITEF]
+
+PROFILE = {"HEADER" : HEADER,
+           "CONFIG_FILES" : CONFIG_FILES,}
+
