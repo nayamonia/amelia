173baf60737c416d550948f40033b0b73fca555a
pierresantana <pierresantana@gmail.com>
2012-12-14 09:15:12 -0200

    NEW: Add pam auth system

--- amelia/resources/login.py
+++ amelia/resources/login.py
@@ -24,6 +24,7 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 import main
 import settings
 import tornado
+import PAM
 
 class LoginHandler(main.BaseHandler):
     def get(self):
@@ -32,11 +33,45 @@ class LoginHandler(main.BaseHandler):
                     next=tornado.escape.url_escape(self.get_argument("next","")))        
     def post(self):
         settings.PAGE_TITLE = "Login"
-        try:
-#            db = MySQLdb.connect(settings.DATABASE["HOST"], 
-#                                 self.get_argument("username"), 
-#                                 self.get_argument("password"))
-#            db.select_db(settings.DATABASE["DATABASE"])
+        if self.is_authorized(self.get_argument("username"),self.get_argument("password")):
             self.set_secure_cookie("username", self.get_argument("username"), 1)
-        finally:
             self.redirect(self.get_argument("next", "/"))
+        else:
+            """ Fix this code and show the error message... """
+            self.redirect(self.get_argument("next", "/?err=InvalidUser"))
+        
+
+    def is_authorized(self, username, password):
+        """
+        Returns true is a user is authorised via PAM.
+    
+        Note: We use the 'login' PAM stack rather than inventing our own.
+        """
+        pam_auth = PAM.pam()
+    
+        pam_auth.start('login')
+        pam_auth.set_item(PAM.PAM_USER, username)
+        pam_auth.set_item(PAM.PAM_TTY, 'console')
+    
+        def _pam_conv(auth, query_list, user_data=None):
+            resp = []
+            for i in range(len(query_list)):
+                query, qtype = query_list[i]
+                if qtype == PAM.PAM_PROMPT_ECHO_ON:
+                    resp.append((username, 0))
+                elif qtype == PAM.PAM_PROMPT_ECHO_OFF:
+                    resp.append((password, 0))
+                else:
+                    return None
+            return resp
+    
+        pam_auth.set_item(PAM.PAM_CONV, _pam_conv)
+    
+        try:
+            pam_auth.authenticate()
+            pam_auth.acct_mgmt()
+        except Exception, e:
+            print 'Error with PAM: %s' % e
+            return False
+        else:
+            return True
